f9268df3f64edfdc090d9be6a0ceea85b4cb3b15
sobinsibichen <124669405+sobinsibichen@users.noreply.github.com>
2023-06-11 02:56:05 -0700

    Add files via upload

---
+++ Registration form/Registration form/Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(273, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(322, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "Registration Form"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(41, 145)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(97, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "Student ID"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(41, 209)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(97, 23)
+        Label3.TabIndex = 2
+        Label3.Text = "First Name"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(41, 274)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(94, 23)
+        Label4.TabIndex = 3
+        Label4.Text = "Last Name"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(44, 338)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(75, 23)
+        Label5.TabIndex = 4
+        Label5.Text = "Email Id"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(44, 402)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(68, 23)
+        Label6.TabIndex = 5
+        Label6.Text = "Gender"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(63, 480)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(120, 53)
+        Button1.TabIndex = 6
+        Button1.Text = "Insert"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(250, 480)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(120, 53)
+        Button2.TabIndex = 7
+        Button2.Text = "New"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(456, 480)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(120, 53)
+        Button3.TabIndex = 8
+        Button3.Text = "Update"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button4.Location = New Point(678, 480)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(120, 53)
+        Button4.TabIndex = 9
+        Button4.Text = "Delete"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.Location = New Point(678, 562)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(120, 42)
+        Button5.TabIndex = 10
+        Button5.Text = "Export"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(199, 141)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(205, 27)
+        TextBox1.TabIndex = 11
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(199, 205)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(205, 27)
+        TextBox2.TabIndex = 12
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(199, 270)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(205, 27)
+        TextBox3.TabIndex = 13
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(199, 334)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(205, 27)
+        TextBox4.TabIndex = 14
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(199, 398)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(205, 27)
+        TextBox5.TabIndex = 15
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(410, 140)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(503, 285)
+        DataGridView1.TabIndex = 16
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(933, 637)
+        Controls.Add(DataGridView1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Registration Form"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Registration form/Registration form/Form1.vb
@@ -0,0 +1,142 @@
+﻿
+Imports System.IO
+Imports System.Windows
+
+Public Class Form1
+    Dim table As New DataTable("table")
+    Dim index As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        table.Columns.Add("ID", Type.GetType("System.Int32"))
+
+        table.Columns.Add("First Name", Type.GetType("System.String"))
+
+        table.Columns.Add("Last Name", Type.GetType("System.String"))
+
+        table.Columns.Add("Email", Type.GetType("System.String"))
+
+        table.Columns.Add("Gender", Type.GetType("System.String"))
+
+        DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        table.Rows.Add(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        index = e.RowIndex
+        Dim selectedrow As DataGridViewRow
+
+        selectedrow = DataGridView1.Rows(index)
+
+        TextBox1.Text = selectedrow.Cells(0).Value.ToString
+
+        TextBox2.Text = selectedrow.Cells(1).Value.ToString
+
+        TextBox3.Text = selectedrow.Cells(2).Value.ToString
+
+        TextBox4.Text = selectedrow.Cells(3).Value.ToString
+
+        TextBox5.Text = selectedrow.Cells(4).Value.ToString
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim newdata As DataGridViewRow
+        newdata = DataGridView1.Rows(index)
+
+        newdata.Cells(0).Value = TextBox1.Text
+
+
+        newdata.Cells(1).Value = TextBox2.Text
+
+
+        newdata.Cells(2).Value = TextBox3.Text
+
+
+        newdata.Cells(3).Value = TextBox4.Text
+
+        newdata.Cells(4).Value = TextBox5.Text
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        DataGridView1.Rows.RemoveAt(index)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim writer As New StreamWriter("D:\sample projects\Registration form\Exported data.txt")
+
+        For i1 As Integer = 0 To DataGridView1.Rows.Count - 2 Step +1
+
+            For j1 As Integer = 0 To DataGridView1.Columns.Count - 1 Step +1
+
+                ' if last column
+                If j1 = DataGridView1.Columns.Count - 1 Then
+                    writer.Write(vbTab & DataGridView1.Rows(i1).Cells(j1).Value.ToString())
+                Else
+                    writer.Write(vbTab & DataGridView1.Rows(i1).Cells(j1).Value.ToString() & vbTab & "|")
+                End If
+
+
+            Next j1
+
+            writer.WriteLine("")
+
+        Next i1
+
+        writer.Close()
+
+
+
+        Dim xlApp As Microsoft.Office.Interop.Excel.Application
+        Dim xlWorkBook As Microsoft.Office.Interop.Excel.Workbook
+        Dim xlWorkSheet As Microsoft.Office.Interop.Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+        Dim i As Integer
+        Dim j As Integer
+        xlApp = New Microsoft.Office.Interop.Excel.Application
+        xlWorkBook = xlApp.Workbooks.Add(misValue)
+        xlWorkSheet = xlWorkBook.Sheets("sheet1")
+        For i = 0 To DataGridView1.RowCount - 2
+            For j = 0 To DataGridView1.ColumnCount - 1
+                For k As Integer = 1 To DataGridView1.Columns.Count
+                    xlWorkSheet.Cells(1, k) = DataGridView1.Columns(k - 1).HeaderText
+                    xlWorkSheet.Cells(i + 2, j + 1) = DataGridView1(j, i).Value.ToString()
+                Next
+            Next
+        Next
+        xlWorkSheet.SaveAs("D:\sample projects\Registration form\exported.xlsx")
+        xlWorkBook.Close()
+        xlApp.Quit()
+        releaseObject(xlApp)
+        releaseObject(xlWorkBook)
+        releaseObject(xlWorkSheet)
+
+        MessageBox.Show("Data Exported")
+
+
+    End Sub
+
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+End Class
